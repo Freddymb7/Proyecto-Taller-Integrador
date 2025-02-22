54fd99b3228b011d950666311653e92362ee43e9
Gabriel <gonzalezchavezgabriel79@gmail.com>
2025-02-22 17:14:40 -0600

    arrangemet

---
+++ Front-end /tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx}",
+      "./src/App.jsx"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
