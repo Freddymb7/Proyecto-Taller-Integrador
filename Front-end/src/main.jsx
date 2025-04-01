9b9219782df9df0dcdebae9fb37401d4c4fcf764
Jeaustin Paniagua <72481551+JPaniaguaTec@users.noreply.github.com>
2025-04-01 01:50:21 -0600

    Update main.jsx
    Actualización

--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Login from './components/Login';
+import ProtectedRoute from './components/ProtectedRoute';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <App />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
+);
